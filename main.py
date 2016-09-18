a805d1ee32df3fcec2c52367a8cbc92172198041
Dan Svensson <dan.svensson@outlook.com>
2016-09-18 22:03:18 +0200

    Commented out pages in the url list generator

--- main.py
+++ main.py
@@ -112,21 +112,30 @@ class db:
     def crafturllist(self):
         print("Crafting URL list...")
         self.selectURL()
-        url = self.dbcu.fetchone()[0]
+
+        # url = self.dbcu.fetchone()[0]
+        urls = self.dbcu.fetchall()
         # print("URL IS ::", url)
-        self.selectID(url)
-        id = str(self.dbcu.fetchone()[0])
+        # self.selectID(url)
+        #id = str(self.dbcu.fetchone()[0])
         # print("ID IS :: ", id)
-        self.selectPage(id)
-        pages = self.dbcu.fetchall()
+        # self.selectPage(id)
+        #pages = self.dbcu.fetchall()
         # print(pages)
         urllist = list()
-        for i in pages:
+        # for i in pages:
+        #     # rint(i)
+        #     # print(url + i[0])
+        #     # print()
+        #     urllist.append("http://" + url + i[0])
+
+        for i in urls:
             # rint(i)
-            # print(url + i[0])
+            print(i)
             # print()
-            urllist.append("http://" + url + i[0])
-        # print("URl= ", urllist)
+            urllist.append("http://" + i[0])  # + i[0]) # Commented out pages table br
+        print("URl= ", urllist)
+        #exit()
         print("URLlist Done!")
         return urllist
 
@@ -282,85 +291,3 @@ for each in x.a.crafturllist():
 # x.index('http://www.iana.org')
 
 #x.a.dbdump()
-
-
-# with urllib.request.urlopen(url) as f:
-#     now = datetime.datetime.now()
-#     a = urllib.parse.urlparse(url.full_url)
-#     s = (None, now, a[0], a[1], 0)
-#     c.execute('INSERT INTO urls VALUES (?,?,?,?,?)', s)
-#     #print("Adding ? to database",s)
-#
-#     dbconn.commit()
-#
-#    # print(c.execute("SELECT id FROM urls WHERE netloc = 'www.iana.org'"))
-#     if a[2] != "":
-#         #print("Found macth: " + a[2])
-#         a2 = (a[1],)
-#         #print(a2)
-#         c.execute('SELECT id FROM urls WHERE netloc=?', a2)
-#         g = c.fetchone()
-#         #print(g)
-#         s = (None,a[2],0,g[0])
-#         c.execute('INSERT INTO pages VALUES (?,?,?,?)', s)
-#
-#         # c.execute('SELECT * FROM pages')
-#         # g = c.fetchall()
-#         # for line in g:
-#         #     for a in line:
-#         #         print(a)
-#
-#     site = f.read().decode('utf-8')
-#     #print(site)
-#     #print(r.findall(site))
-#     for each in r.findall(site):
-#         #print(each)
-#         now = datetime.datetime.now()
-#         a = urllib.parse.urlparse(each)
-#         s = (None, now, a[0], a[1], 0)
-#         if a[1] != '':
-#             try:
-#                 c.execute('INSERT INTO urls VALUES (?,?,?,?,?)', s)
-#                 #print("Adding ? to database", s)
-#
-#             except sqlite3.IntegrityError as error:
-#                 print("Exeption sqlite3.IntegrityError: ", error, "\nFor data: ", s)
-#                 dbconn.rollback()
-#                 pass
-#
-#
-#
-#         dbconn.commit()
-#
-#         if a[2] != "":
-#             if a[1] == '':
-#                 a3 = urllib.parse.urlparse(url.full_url)
-#             else:
-#                 a3 = a
-#             #print(a[2])
-#             a2 = (a3[1],)
-#             #print(a2)
-#             c.execute('SELECT id FROM urls WHERE netloc=?', a2)
-#             g = c.fetchone()
-#             while g != None:
-#                 s = (None, a[2], 0, g[0])
-#                 print("adding: ", s, "to db")
-#                 c.execute('INSERT INTO pages VALUES (?,?,?,?)', s)
-#                 g = c.fetchone()
-#
-#     a = urllib.parse.urlparse(url.full_url)
-#     a2 = (a[1],)
-#     c.execute('UPDATE urls SET indexed = 1 where netloc = ?', a2)
-#     dbconn.commit()
-#
-# c.execute('SELECT * FROM urls')
-# g = c.fetchall()
-# for line in g:
-#     for a in line:
-#         print(a)
-#
-# c.execute('SELECT * FROM pages')
-# g = c.fetchall()
-# for line in g:
-#     for a in line:
-#         print(a)
